009a60be25c67f4494ff6c1c6ffc2db13bb62a45
pverrier <pro@pverrier.fr>
2014-12-30 15:12:30 +0100

    Premier commit

---
+++ lang/paquet-kitcnrs_glu_seminaire_en.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+  'kitcnrs_glu_seminaire_nom' => 'CNRS template -glue- Seminar',
+  
+  'kitcnrs_glu_seminaire_slogan' => 'Glue code',
+  
+  'kitcnrs_glu_seminaire_description' => 'Allows [Seminar->http://contrib.spip.net/Seminaire-v2] plugin to display content inside the [CNRS template->http://www.harmoweb.cnrs.fr/spip.php?article181] skin.',
+
+);
+
+?>
---
+++ lang/paquet-kitcnrs_glu_seminaire_fr.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+  'kitcnrs_glu_seminaire_nom' => 'Kit CNRS -glu- Séminaire',
+
+  'kitcnrs_glu_seminaire_slogan' => 'Plugin de liaison',
+
+  'kitcnrs_glu_seminaire_description' => 'Permet au plugin [Séminaire->http://contrib.spip.net/Seminaire-v2] d\'afficher son contenu au sein de l\'habillage du [Kit labos CNRS->http://www.harmoweb.cnrs.fr/spip.php?article181].',
+
+);
+
+?>
